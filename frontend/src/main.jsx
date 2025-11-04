4f46efded795e325f85f3cc5e0ec63bace7586eb
Nishika-git <nishikalab@gmail.com>
2025-11-04 16:51:12 +0530

    fix: login details

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,14 +5,12 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import { Auth0Provider } from '@auth0/auth0-react';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
-    domain="dev-nishika28.us.auth0.com"
-    clientId="94WhBs2BKEfGEe7TwlkNrUlmdeej0qxX"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-  <AuthProvider>
-    <App />
-  </AuthProvider>
-  </Auth0Provider>
+  domain={import.meta.env.VITE_AUTH0_DOMAIN}
+  clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+  authorizationParams={{
+    redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+  }}
+>
+  <App />
+</Auth0Provider>
 );
